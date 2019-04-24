32456dbb027e0bc42cc8822eab30cedbd06521e3
Louise Hyldal Thingholm Hansen <loui629f@edu.eal.dk>
2019-04-24 12:49:28 +0200

    Tabs

---
+++ Eventkoordination - Eksamen/TabWindowAdmin.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Eventkoordination___Eksamen
+{
+	/// <summary>
+	/// Interaction logic for TabWindowAdmin.xaml
+	/// </summary>
+	public partial class TabWindowAdmin : Window
+	{
+		public TabWindowAdmin()
+		{
+			InitializeComponent();
+		}
+	}
+}
