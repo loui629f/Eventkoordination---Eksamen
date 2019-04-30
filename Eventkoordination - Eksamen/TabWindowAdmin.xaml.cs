0ff2ea89ece732d85cc14070613e70c52ae4b025
Nicolai Mogensen <nico6519@edu.eal.dk>
2019-04-30 14:09:49 +0200

    Statistik

--- Eventkoordination - Eksamen/TabWindowAdmin.xaml.cs
+++ Eventkoordination - Eksamen/TabWindowAdmin.xaml.cs
@@ -11,18 +11,30 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Controls.DataVisualization;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Collections.ObjectModel;
+
 
 namespace Eventkoordination___Eksamen
 {
-	/// <summary>
-	/// Interaction logic for TabWindowAdmin.xaml
-	/// </summary>
-	public partial class TabWindowAdmin : Window
-	{
-		public TabWindowAdmin()
-		{
-			InitializeComponent();
-		}
+    /// <summary>
+    /// Interaction logic for TabWindowAdmin.xaml
+    /// </summary>
+    public partial class TabWindowAdmin : Window
+    {
+
+
+
+        public TabWindowAdmin()
+        {
+            InitializeComponent();
+
+
+
+
+
+        }
 
         private void BtnClickSlet(object sender, RoutedEventArgs e)
         {
@@ -46,5 +58,33 @@ namespace Eventkoordination___Eksamen
         {
 
         }
+
+
+
+
+
     }
+    public class Statistik
+    {
+        public string Name { get; set; }
+        public Int16 Share { get; set; }
+
+
+    }
+
+    public class StatistikCollection : System.Collections.ObjectModel.Collection<Statistik>
+    {
+        public StatistikCollection()
+        {
+            Add(new Statistik { Name = "oprydning", Share = 25 });
+            Add(new Statistik { Name = "Fest", Share = 25 });
+            Add(new Statistik { Name = "Opstilling", Share = 25 });
+            Add(new Statistik { Name = "Andet", Share = 25 });
+
+
+        }
+
+
+    }
+
 }
