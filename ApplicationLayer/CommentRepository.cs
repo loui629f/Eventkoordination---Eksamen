f9e708ec347e74d308bb39c7c4cf8d3231afc973
Louise Hyldal Thingholm Hansen <loui629f@edu.eal.dk>
2019-05-20 13:16:13 +0200

    repos

--- ApplicationLayer/CommentRepository.cs
+++ ApplicationLayer/CommentRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainLayer;
 
 namespace ApplicationLayer
 {
     class CommentRepository
     {
-    }
+		public List<Comment> Comments { get; private set; } = new List<Comment>();
+	}
 }
--- ApplicationLayer/UserRepository.cs
+++ ApplicationLayer/UserRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainLayer;
 
 namespace ApplicationLayer
 {
     class UserRepository
     {
-    }
+		public List<User> Users { get; private set; } = new List<User>();
+	}
 }
